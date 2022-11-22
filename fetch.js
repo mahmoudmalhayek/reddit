b0b84c6f759e15b3818581c4477977b2fc990e19
Mahmoud Alhayek <81908655+mahmoudmalhayek@users.noreply.github.com>
2022-11-22 13:36:23 +0200

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -1,22 +1,22 @@
-fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-10-20&sortBy=publishedAt&apiKey=d63e1c979b1e49baac598260bfb10650')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-            document.getElementById("news-title1").innerHTML += data[0].title;
-            document.getElementById("news-description1").innerHTML += data[0].description;
-            document.getElementById("news-author1").innerHTML += data[0].author;
+fetch('https://newsapi.org/v2/everything?q=apple&from=2022-11-19&to=2022-11-19&sortBy=popularity&apiKey=d63e1c979b1e49baac598260bfb10650')
+.then(response => response.json())
+// .then(data => console.log(data.articles[0].title))
+.then((data) => {
+            
+            document.getElementById("news-title1").innerText += (data.articles[0].title).substring(0,30);
+            document.getElementById("news-description1").innerText += data.articles[0].description.substring(0,48);
+            document.getElementById("news-author1").innerText += data.articles[0].author.substring(0,20);
 
-            document.getElementById("news-title2").innerHTML += data[1].title;
-            document.getElementById("news-description2").innerHTML += data[1].description;
-            document.getElementById("news-author2").innerHTML += data[1].author;
+            document.getElementById("news-title2").innerHTML += data.articles[1].title.substring(0,30);
+            document.getElementById("news-description2").innerHTML += data.articles[1].description.substring(0,48);
+            document.getElementById("news-author2").innerHTML += data.articles[1].author.substring(0,20);
 
-            document.getElementById("news-title3").innerHTML += data[2].title;
-            document.getElementById("news-description3").innerHTML += data[2].description;
-            document.getElementById("news-author3").innerHTML += data[2].author;
+            document.getElementById("news-title3").innerHTML += data.articles[2].title.substring(0,30);
+            document.getElementById("news-description3").innerHTML += data.articles[2].description.substring(0,48);
+            document.getElementById("news-author3").innerHTML += data.articles[2].author.substring(0,20);
 
-            document.getElementById("news-title4").innerHTML += data[3].title;
-            document.getElementById("news-description4").innerHTML += data[3].description;
-            document.getElementById("news-author4").innerHTML += data[3].author;
+            document.getElementById("news-title4").innerHTML += data.articles[3].title.substring(0,30);
+            document.getElementById("news-description4").innerHTML += data.articles[3].description.substring(0,48);
+            document.getElementById("news-author4").innerHTML += data.articles[3].author.substring(0,20);
         
-    })
+})
